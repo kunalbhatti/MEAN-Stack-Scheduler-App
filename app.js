cfa0e980f1ef055f92d8ce0d11e6bda63edec977
Kunal Bhatti <kunal.bhatti15@gmail.com>
2021-01-30 17:01:28 +0530

    ''

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
+
 app.use('/auth', authController);
 app.use('/app', todoController);
 
