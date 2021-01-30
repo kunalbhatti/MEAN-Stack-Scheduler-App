936467ffdfd475f78456f8c263bd52ff758a517c
Kunal Bhatti <kunal.bhatti15@gmail.com>
2021-01-30 17:17:11 +0530

    making node server to serve static files from dist folder

--- app.js
+++ app.js
@@ -18,20 +18,22 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-const forceSSL = function() {
+const forceSSL = function () {
     return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
     }
-  }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist'));
 
 app.use('/auth', authController);
 app.use('/app', todoController);
