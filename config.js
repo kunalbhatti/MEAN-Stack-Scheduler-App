02e4f6e799410702823e4a315229b74d14eb1497
Kunal Bhatti <32214410+kunalbhatti@users.noreply.github.com>
2021-02-02 12:35:12 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'secret': 'secret-key',
-    'api_key': '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',
-    'from': 'admin@howtogizmo.com',
-    'db_link': 'mongodb+srv://kunal-bhatti:uSsAJ0EGeEdeuDM1@cluster0.3o6sp.mongodb.net/todo?retryWrites=true&w=majority'
-}
